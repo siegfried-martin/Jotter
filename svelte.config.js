65cfd4f86278d5b0cf21b544088779e95e982f99
siegfried-martin <siegfried.martin77@gmail.com>
2025-07-17 01:20:31 -0400

    use strictly node server

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({ out: 'build' })
+	},
 	compilerOptions: {
 		accessors: false
 	}
